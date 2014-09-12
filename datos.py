71dc8fbd5b6035c71f80e96a0a3ae32ebf3b9318
ericq1 <erickmartin.2@gmail.com>
2014-09-12 02:08:01 -0500

    Create datos.py

---
+++ datos.py
@@ -0,0 +1,27 @@
+# Tipos de datos y operadores
+b = True
+n = True
+n = not (n)
+e = 4
+s = e + 4
+d = s + 2.25
+r = round (d)
+r = float(r)
+di = d / 1.15 
+k = int(di)
+res = 5 % 2
+div1 = 4 / 2
+div1 = int(div1)
+div2 =  4 / 3
+div2 = int(div2)
+el = 9845 ** 24
+l = ("hola variable");
+ll = l + (" tipo cadena");
+pi = 3.1416
+area = r**2*pi
+gr = 1.8*12+32
+gr = int(gr)
+imc = 72/1.85**2
+print(b);print(n);print(e);print(s);print(d);print(r);print(di);print(k);print(res);print(div1);print(div2);print(el);print(l);print(ll);print(area);print(gr);print(imc)
+# Eric Calzadilla
+# A01560889
